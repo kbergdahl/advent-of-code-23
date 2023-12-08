cac2dbeec403c12fd80512573dc385b11bd1b035
kbergdahl <bergdahl.karin@gmail.com>
2023-12-08 20:01:39 +0100

    Day 8 started

--- day8/day8.py
+++ day8/day8.py
@@ -1,3 +1,12 @@
 # Read data
-#data_source = 'day7/example_data.txt'
-data_source = 'day7/data.txt'
+data_source = 'day8/example_data.txt'
+#data_source = 'day8/data.txt'
+
+with open(data_source) as f:
+    lines = f.read().split('\n')
+
+
+# Process data
+instructions = lines[0]
+paths = {line[0:3]: (line[7:10], line[12:15]) for line in lines[2:]}
+
